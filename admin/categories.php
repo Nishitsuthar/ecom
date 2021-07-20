7fb52deaaa37f9490068d8b3e871da96d556356f
Nishit Suthar <nishitsuthar123@gmail.com>
2021-07-20 16:02:52 +0530

    crud operation done in categories department

--- admin/categories.php
+++ admin/categories.php
@@ -1,7 +1,4 @@
 <?php
-
-use function PHPSTORM_META\type;
-
 require('top.inc.php');
 
 if (isset($_GET['type']) && $_GET['type'] != '') {
@@ -14,15 +11,20 @@ if (isset($_GET['type']) && $_GET['type'] != '') {
         } else {
             $status = '0';
         }
-        $update_status = "update categories set status='$status' where id='$id'";
-        mysqli_query($con, $update_status);
+        $update_status_sql = "update categories set status='$status' where id='$id'";
+        mysqli_query($con, $update_status_sql);
+    }
+
+    if ($type == 'delete') {
+        $id = get_safe_value($con, $_GET['id']);
+        $delete_sql = "delete from categories where id='$id'";
+        mysqli_query($con, $delete_sql);
     }
 }
 
-$sql = " SELECT * FROM categories order by categories asc ";
+$sql = "select * from categories order by categories asc";
 $res = mysqli_query($con, $sql);
 ?>
-
 <div class="content pb-0">
     <div class="orders">
         <div class="row">
@@ -30,6 +32,7 @@ $res = mysqli_query($con, $sql);
                 <div class="card">
                     <div class="card-body">
                         <h4 class="box-title">Categories </h4>
+                        <h4 class="box-link"><a href="manage_categories.php">Add Categories</a> </h4>
                     </div>
                     <div class="card-body--">
                         <div class="table-stats order-table ov-h">
@@ -39,7 +42,7 @@ $res = mysqli_query($con, $sql);
                                         <th class="serial">#</th>
                                         <th>ID</th>
                                         <th>Categories</th>
-                                        <th>Status</th>
+                                        <th></th>
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -47,14 +50,22 @@ $res = mysqli_query($con, $sql);
                                     $i = 1;
                                     while ($row = mysqli_fetch_assoc($res)) { ?>
                                         <tr>
-                                            <td class='serial'><?php echo $i ?></td>
+                                            <td class="serial"><?php echo $i ?></td>
                                             <td><?php echo $row['id'] ?></td>
                                             <td><?php echo $row['categories'] ?></td>
-                                            <td><?php if ($row['status'] == 1) {
-                                                    echo "<a href='?type=status&operation=deactivate&id=" . $row['id'] . "'>Active</a>";
+                                            <td>
+                                                <?php
+                                                if ($row['status'] == 1) {
+                                                    echo "<span class='badge badge-complete'><a href='?type=status&operation=deactive&id=" . $row['id'] . "'>Active</a></span>&nbsp;";
                                                 } else {
-                                                    echo "<a href='?type=status&operation=activate&id=" . $row['id'] . "'>Deactive</a>";
-                                                } ?></td>
+                                                    echo "<span class='badge badge-pending'><a href='?type=status&operation=active&id=" . $row['id'] . "'>Deactive</a></span>&nbsp;";
+                                                }
+                                                echo "<span class='badge badge-edit'><a href='manage_categories.php?id=" . $row['id'] . "'>Edit</a></span>&nbsp;";
+
+                                                echo "<span class='badge badge-delete'><a href='?type=delete&id=" . $row['id'] . "'>Delete</a></span>";
+
+                                                ?>
+                                            </td>
                                         </tr>
                                     <?php } ?>
                                 </tbody>
@@ -66,4 +77,6 @@ $res = mysqli_query($con, $sql);
         </div>
     </div>
 </div>
-<?php require('footer.inc.php'); ?>
+<?php
+require('footer.inc.php');
+?>
